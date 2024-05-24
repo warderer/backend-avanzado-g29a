10184a2abc5c60e4c0e30a0714b05bb4c6b6c757
César Guerra <warderer@gmail.com>
2024-05-23 18:56:12 -0600

    ✨ Add getAllCars to Controller and Router

--- 02-cars-api/controllers/carController.js
+++ 02-cars-api/controllers/carController.js
@@ -9,10 +9,24 @@ const createCar = async (req, res) => {
     res.status(400).json({ message: `Error Creating Car: ${error}` })
   }
 }
+
 // Read
+
+// Get all cars
+const getAllCars = async (req, res) => {
+  try {
+    const cars = await Car.find() // equivalente a SELECT * FROM cars
+    res.status(200).json(cars)
+  } catch (error) {
+    res.status(400).json({ message: `Error Getting Cars: ${error}` })
+  }
+}
 
 // Update
 
 // Delete
 
-export { createCar }
+export {
+  createCar,
+  getAllCars
+}
--- 02-cars-api/routes/carRoutes.js
+++ 02-cars-api/routes/carRoutes.js
@@ -4,5 +4,6 @@ import * as carController from '../controllers/carController.js'
 const carRoutes = express.Router()
 
 carRoutes.post('/cars', carController.createCar)
+carRoutes.get('/cars', carController.getAllCars)
 
 export default carRoutes
