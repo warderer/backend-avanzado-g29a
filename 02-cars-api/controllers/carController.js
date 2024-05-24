c6fce4a59c47d2dbaf849d1940532344a47c25e2
César Guerra <warderer@gmail.com>
2024-05-23 20:04:09 -0600

    ✨ Add deleteCarById Controller and Route

--- 02-cars-api/controllers/carController.js
+++ 02-cars-api/controllers/carController.js
@@ -15,7 +15,7 @@ const createCar = async (req, res) => {
 // Get all cars
 const getAllCars = async (req, res) => {
   try {
-    const cars = await Car.find() // equivalente a SELECT * FROM cars
+    const cars = await Car.find({ isActive: true }) // equivalente a SELECT * FROM cars WHERE isActive = true
     res.status(200).json(cars)
   } catch (error) {
     res.status(400).json({ message: `Error Getting Cars: ${error}` })
@@ -24,7 +24,7 @@ const getAllCars = async (req, res) => {
 
 const getCarById = async (req, res) => {
   try {
-    const car = await Car.findById(req.params.id) // equivalente a SELECT * FROM cars WHERE id = req.params.id
+    const car = await Car.find({ _id: req.params.id, isActive: true })
     res.status(200).json(car)
   } catch (error) {
     res.status(400).json({ message: `Error Getting Car: ${error}` })
@@ -45,10 +45,41 @@ const updateCarById = async (req, res) => {
 }
 
 // Delete
+const deleteCarById = async (req, res) => {
+  if (!req.params.id) {
+    return res.status(400).json({ message: 'Car ID is required' })
+  }
+
+  // Borrado físico: Si el query string ?destroy=true, se borra el carro realmente de la base de datos.
+  if (req.query.destroy === 'true') {
+    try {
+      const deletedCar = await Car.findByIdAndDelete(req.params.id) // Si el id no existe findByIdAndDelete devuelve null
+      if (deletedCar === null) {
+        return res.status(404).json({ message: 'Car Not Found for Delete' })
+      }
+      return res.status(204).json() // 204: No Content
+    } catch (error) {
+      return res.status(400).json({ message: `Error Deleting Car: ${error}` })
+    }
+  }
+
+  // Borrado lógico: Si no se envía el query string ?destroy=true, se cambia el campo isActive a false.
+  try {
+    const updatedCar = await Car.findByIdAndUpdate(req.params.id, { isActive: false }, { new: false })
+    // Solicite que se devuelva el documento antes de la actualización (el original) para verificar si el carro existe y si esta activo.
+    if (updatedCar === null || updatedCar.isActive === false) {
+      return res.status(404).json({ message: 'Delete: Car Not Found' })
+    }
+    return res.status(204).json() // 204: No Content
+  } catch (error) {
+    return res.status(400).json({ message: `Error Deleting Car: ${error}` })
+  }
+}
 
 export {
   createCar,
   getAllCars,
   getCarById,
-  updateCarById
+  updateCarById,
+  deleteCarById
 }
--- 02-cars-api/routes/carRoutes.js
+++ 02-cars-api/routes/carRoutes.js
@@ -7,5 +7,6 @@ carRoutes.post('/cars', carController.createCar)
 carRoutes.get('/cars', carController.getAllCars)
 carRoutes.get('/cars/:id', carController.getCarById)
 carRoutes.patch('/cars/:id', carController.updateCarById)
+carRoutes.delete('/cars/:id', carController.deleteCarById)
 
 export default carRoutes
