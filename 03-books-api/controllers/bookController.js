9176b8b5de70abe75b9a4db57509d1d9dff464c5
César Guerra <warderer@gmail.com>
2024-05-30 19:35:33 -0600

    ✨ Add updateBookById controller and route

--- 03-books-api/controllers/bookController.js
+++ 03-books-api/controllers/bookController.js
@@ -79,7 +79,25 @@ const getBookById = async (req, res) => {
 }
 
 // UPDATE
+const updateBookById = async (req, res) => {
+  // Valido que el ID sea un ObjectID válido de MongoDB (24 caracteres alfanuméricos)
+  if (!req.params.bookId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ error: 'Invalid book ID' })
+  }
+
+  try {
+    const book = await Book
+      .findByIdAndUpdate(req.params.bookId, req.body, { new: true })
+      .populate('authors')
+    if (!book) {
+      return res.status(404).json({ error: 'Cant update: Book not found' })
+    }
+    res.status(200).json(book)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
 
 // DELETE
 
-export { createBook, getAllBooks, getBookById }
+export { createBook, getAllBooks, getBookById, updateBookById }
