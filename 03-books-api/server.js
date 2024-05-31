cce156646bc06e913a6481ea22c738833f3e6cdc
César Guerra <warderer@gmail.com>
2024-05-30 21:03:56 -0600

    ✨ Add Morgan http logger to server

--- 03-books-api/server.js
+++ 03-books-api/server.js
@@ -1,11 +1,20 @@
 import { connect } from './config/database.js'
 import express from 'express'
+import morgan from 'morgan'
 import bookRoutes from './routes/bookRoutes.js'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
+
+// tokens de morgan personalizados
+morgan.token('body', (req) => JSON.stringify(req.body))
+morgan.token('host', (req) => req.hostname)
+morgan.token('param', (req, res, param) => req.params ? req.params[param] : null)
+
+// app.use(morgan('tiny')) // usamos morgan para loggear las peticiones HTTP
+app.use(morgan(':host :method :url :status :param[id] - :response-time ms - :date - :body'))
 
 // Aquí van las rutas
 app.use('/api/v1/books', bookRoutes)
